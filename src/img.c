5dbb72826ed9124ceb76e61a4692cb399a32c534
rick <rick@ricks-MacBook-Air.local>
2022-08-21 22:20:20 -0400

    add header and dimensions function

--- src/img.c
+++ src/img.c
@@ -5,73 +5,67 @@
 // Copyright (c) 2011 TJ Holowaychuk <tj@vision-media.ca>
 //
 
-#include <stdio.h>
-#include <stdint.h>
-#include <stdlib.h>
 #include <fcntl.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 /*
  * Program version.
  */
 
-#define VERSION "0.0.2"
+#define VERSION    "0.0.2"
 
 /*
  * Sniff bytes for JPEG's magic number ff d8.
  */
 
-int
-is_jpeg(uint8_t *buf) {
-  return 0xff == buf[0] && 0xd8 == buf[1];
+int is_jpeg(uint8_t *buf) {
+  return(0xff == buf[0] && 0xd8 == buf[1]);
 }
 
 /*
  * Sniff bytes 0..2 for "GIF".
  */
 
-int
-is_gif(uint8_t *buf) {
-  return 'G' == buf[0] && 'I' == buf[1] && 'F' == buf[2];
+int is_gif(uint8_t *buf) {
+  return('G' == buf[0] && 'I' == buf[1] && 'F' == buf[2]);
 }
 
 /*
  * Sniff bytes 1..3 for "PNG".
  */
 
-int
-is_png(uint8_t *buf) {
-  return 'P' == buf[1] && 'N' == buf[2] && 'G' == buf[3];
+int is_png(uint8_t *buf) {
+  return('P' == buf[1] && 'N' == buf[2] && 'G' == buf[3]);
 }
 
 /*
  * Read big-endian uint32 from `buf`.
  */
 
-int
-read_uint32(uint8_t *buf) {
-  return buf[0] << 24
-    | buf[1] << 16
-    | buf[2] << 8
-    | buf[3];
+int read_uint32(uint8_t *buf) {
+  return(buf[0] << 24
+         | buf[1] << 16
+         | buf[2] << 8
+         | buf[3]);
 }
 
 /*
  * Read little-endian uint16 from `buf`.
  */
 
-int
-read_uint16(uint8_t *buf) {
-  return buf[1] << 8
-    | buf[0];
+int read_uint16(uint8_t *buf) {
+  return(buf[1] << 8
+         | buf[0]);
 }
 
 /*
  * Output CSS for `width` / `height`.
  */
 
-void
-output_css_dimensions(int width, int height) {
+void output_css_dimensions(int width, int height) {
   printf("width: %dpx;\n", width);
   printf("height: %dpx;\n", height);
 }
@@ -80,15 +74,15 @@ output_css_dimensions(int width, int height) {
  * Read `len` bytes at the given `offset` into `buf`.
  */
 
-void
-read_from(const char *path, char *buf, int len, int offset) {
+void read_from(const char *path, char *buf, int len, int offset) {
   // open file
   int fd = open(path, O_RDONLY);
+
   if (fd < 0) {
     perror("open()");
     exit(1);
   }
- 
+
   // read bytes
   if (pread(fd, buf, len, offset) < 0) {
     perror("read()");
@@ -101,35 +95,48 @@ read_from(const char *path, char *buf, int len, int offset) {
  * Output GIF dimensions for `path`.
  */
 
-void
-output_gif_dimensions(const char *path) {
+void output_gif_dimensions(const char *path) {
   char buf[4];
+
   read_from(path, buf, 4, 6);
   output_css_dimensions(
-      read_uint16(buf)
-    , read_uint16(buf + 2));
+    read_uint16(buf),
+    read_uint16(buf + 2));
 }
 
 /*
  * Output PNG dimensions for `path`.
  */
 
-void
-output_png_dimensions(const char *path) {
-  // read bytes
+struct image_dimensions_t {
+  int width, height;
+};
+
+struct image_dimensions_t *get_png_dimensions(const char *path){
   char buf[8];
+
+  read_from(path, buf, 8, 16);
+  struct image_dimensions_t *d = calloc(1, sizeof(struct image_dimensions_t));
+
+  d->width  = read_uint32(buf);
+  d->height = read_uint32(buf + 4);
+  return(d);
+}
+
+void output_png_dimensions(const char *path) {
+  char buf[8];
+
   read_from(path, buf, 8, 16);
   output_css_dimensions(
-      read_uint32(buf)
-    , read_uint32(buf + 4));
+    read_uint32(buf),
+    read_uint32(buf + 4));
 }
 
 /*
  * Output JPEG dimensions for `path`.
  */
 
-void
-output_jpeg_dimensions(const char *path) {
+void output_jpeg_dimensions(const char *path) {
   fprintf(stderr, "jpeg not yet supported\n");
   exit(1);
 }
@@ -138,8 +145,7 @@ output_jpeg_dimensions(const char *path) {
  * Output usage information.
  */
 
-void
-usage() {
+void usage() {
   printf(
     "\n"
     "  Usage: img <path>\n"
@@ -151,10 +157,11 @@ usage() {
  * Handle args.
  */
 
-int
-main(int argc, const char **argv){
+static int img_main(int argc, const char **argv) {
   // path required
-  if (argc < 2) usage();
+  if (argc < 2) {
+    usage();
+  }
 
   const char *path = argv[1];
 
@@ -174,9 +181,13 @@ main(int argc, const char **argv){
   close(fd);
 
   // sniff bytes
-  if (is_gif(buf)) output_gif_dimensions(path);
-  else if (is_png(buf)) output_png_dimensions(path);
-  else if (is_jpeg(buf)) output_jpeg_dimensions(path);
-  
-  return 0;
+  if (is_gif(buf)) {
+    output_gif_dimensions(path);
+  }else if (is_png(buf)) {
+    output_png_dimensions(path);
+  }else if (is_jpeg(buf)) {
+    output_jpeg_dimensions(path);
+  }
+
+  return(0);
 }
---
+++ src/img.h
@@ -0,0 +1,13 @@
+/* This file was automatically generated.  Do not edit! */
+void usage();
+void output_jpeg_dimensions(const char *path);
+void output_png_dimensions(const char *path);
+struct image_dimensions_t *get_png_dimensions(const char *path);
+void output_gif_dimensions(const char *path);
+void read_from(const char *path, char *buf, int len, int offset);
+void output_css_dimensions(int width, int height);
+int read_uint16(uint8_t *buf);
+int read_uint32(uint8_t *buf);
+int is_png(uint8_t *buf);
+int is_gif(uint8_t *buf);
+int is_jpeg(uint8_t *buf);
